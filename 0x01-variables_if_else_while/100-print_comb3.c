fa367d3593bde0c732f6b3088042004d2d7fb569
officialsundayjoe <berrybur6@outlook.com>
2023-06-16 13:32:44 -0700

    C VARIABLES

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
  * Main - Entry point
- * Description: 'print all possible diffrent combinations of two digits'
+ *
+ * Description: print all possible diffrent
+ * 		 combinations of two digits
+ *
  * Return: always 0
  */
+ 
 int main(void)
 {
 	int digit1 = 0, digit2;
