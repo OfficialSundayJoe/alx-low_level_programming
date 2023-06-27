a812ab135599884cdbbcdd8e304470bcc455175a
OfficialSundayJoe <berrybur6@outlook.com>
2023-06-26 20:59:35 -0700

    C Pointers Array Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,26 +10,11 @@
 void print_rev(char *s)
 {
 
-	int count = 0;
+	int start = 0;
 
-
-	while (count >= 0)
-
-	{
-
-		if (s[count] == '\0')
-
-			break;
-
-		count++;
-
-	}
-
-
-	for (count--; count >= 0; count--)
-
-		_putchar(s[count]);
-
-		_putchar('\n');
-
+	while (s[start])
+		start++;
+	while (start--)
+		_putchar(s[start]);
+	_putchar('\n');
 }
